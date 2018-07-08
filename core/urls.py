7bd5e348d2bba31266835617cb150e0523740489
Pramod Tiwari <you@tpromode@gmai.com>
2018-07-08 16:05:34 +0545

    generating crud for course of app core

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-
 from . import views
+from rest_framework import routers
+from django.conf.urls import url
+
+from django.urls import path, include
+from .views import CourseListView, CourseDetailView, CourseCreateView, CourseUpdateView, CourseDeleteView
+
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
+	path('', CourseListView.as_view(), name='course_list'),
+    path('detail/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
+    path('add/', CourseCreateView.as_view(), name='course_add'),
+    path('edit/<int:pk>/', CourseUpdateView.as_view(), name='course_edit'),
+    path('delete/<int:pk>/', CourseDeleteView.as_view(), name='course_delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,41 @@
-from django.http import HttpResponse
+# from django.http import HttpResponseRidirect
+# from django.shortcuts import get_object_or_404, render
+
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import Course
+from django.urls import reverse_lazy
+from .forms import CourseCreateForm
 
 
-def index(request):
-    return HttpResponse("Hello, world!")
+# Create your views here.
+
+class CourseListView(ListView):
+	model = Course
+	template_name = 'core/course_list.html'
+
+
+class CourseDetailView(DetailView):
+	model = Course
+	template_name = 'core/course_detail.html'
+
+
+class CourseCreateView(CreateView):
+	model = Course
+	#fields = ('name',)
+	form_class = CourseCreateForm
+	template_name = 'core/course_form.html'
+	success_url = reverse_lazy('course_list')
+
+
+class CourseUpdateView(UpdateView):
+	model = Course
+	template_name = 'core/course_form.html'
+	fields = ('name',)
+	success_url = reverse_lazy('course_list')
+
+
+class CourseDeleteView(DeleteView):
+	model = Course
+	template_name = 'core/course_delete.html'
+	success_url = reverse_lazy('course_list')
