ffa5cff0838da7ddee9a10972552291ccba6b5c6
Pramod Tiwari <you@tpromode@gmai.com>
2018-07-17 14:00:53 +0545

    template update

--- core/views.py
+++ core/views.py
@@ -159,4 +159,5 @@ class SubjectUnitView(TemplateView):
 		context = super().get_context_data(**kwargs)
 		context['subject'] = Subject.objects.get(id=self.kwargs['subject_pk'])
 		context['subject_unit'] = Unit.objects.filter(subject_id=self.kwargs['subject_pk'])
+		context['chapters'] = Chapter.objects.filter(unit__subject_id=self.kwargs['subject_pk'])
 		return context
