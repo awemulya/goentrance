c00db605a9a24389e8c70212d69ed06a1cbbb549
awemulya <awemulya@gmail.com>
2018-08-17 11:06:56 +0545

    server url

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://localhost:8000/api/`,
+    baseURL: `http://dotag.naxa.com.np/api/`,
     withCredentials: true,
     headers: {
       'Accept': 'application/json',
